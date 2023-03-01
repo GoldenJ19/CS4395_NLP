450e59143008763cfda74af3e65d4c5d53c9d217
Justin <justinhardy2001@gmail.com>
2023-03-01 05:06:08 -0600

    Assignment 4 - NGLM - LM Build code complete, pickles generated

---
+++ Assignment 4 - N-Gram Language Model/EvaluateLM.py
@@ -0,0 +1,25 @@
+"""
+
+Name: Justin Hardy
+NETID: JEH180008
+Class: Human Language Technologies (CS 4395.001)
+Professor: Dr. Mazidi
+
+"""
+
+
+# Create function(s).
+def main():
+    """
+    This program is designed to...
+
+    :return: exit code (0 = success, 1 = error)
+    """
+
+
+    # Program complete, terminate the program. (exit code 0)
+    exit(0)
+
+
+if __name__ == "__main__":
+    main()
